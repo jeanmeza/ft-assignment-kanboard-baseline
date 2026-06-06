99b912f601f06e43da81366c5182320298a028e1
Jean Meza <meza.jean@proton.me>
2026-06-06 11:22:01 +0200

    add browser container check in kanboard.ps1
    Ensure the Selenium browser container is running before starting Kanboard.

--- kanboard.ps1
+++ kanboard.ps1
@@ -1,10 +1,19 @@
 param(
     [switch]$RunTests,
-    [string]$Test="tests.TestSuite"
+    [string]$Test = "tests.TestSuite"
 )
 
-docker stop kanboard
-docker rm kanboard
+# Check if the Selenium container is running
+$browserRunning = (docker inspect -f "{{.State.Running}}" browser 2> $null)
+if ($browserRunning -ne "true")
+{
+    docker rm browser 2> $null
+    docker run -d -p 4444:4444 -p 7900:7900 --shm-size="2g" --name=browser selenium/standalone-chrome:127.0-chromedriver-127.0
+}
+
+# Start a new Kanboard container
+docker stop kanboard 2> $null
+docker rm kanboard 2> $null
 docker run -d --name kanboard -p 8080:80 -t kanboard/kanboard:v1.2.15
 
 if ($RunTests)
